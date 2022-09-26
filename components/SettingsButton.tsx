df41353c7c9dc5aed15f19e39d9554a9e074cef3
Yumin Yi <yiyumin@gmail.com>
2022-09-26 19:42:43 -0400

    fixing dark mode styling

--- components/SettingsButton.tsx
+++ components/SettingsButton.tsx
@@ -6,7 +6,7 @@ type SettingsButtonProps = {
 const SettingsButton = ({ children, onClick }: SettingsButtonProps) => {
   return (
     <button
-      className='rounded-md border-2 border-black px-2.5 py-1.5 hover:border-amber-600 hover:bg-amber-100 hover:text-amber-900 dark:border-white dark:text-white'
+      className='rounded-md border-2 border-black px-2.5 py-1.5 hover:border-amber-600 hover:bg-amber-100 hover:text-amber-900 dark:border-white dark:text-white dark:hover:border-amber-600 dark:hover:text-amber-900'
       onClick={onClick}
     >
       {children}
